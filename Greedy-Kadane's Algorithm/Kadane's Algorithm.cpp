93214e5a3ae2ed08c2633383b9d87ddab49c5e5a
PUSHPENDRA KUMAR <94159743+Pushpendra1723@users.noreply.github.com>
2023-06-01 13:19:41 +0530

    Add files via upload

---
+++ Greedy-Kadane's Algorithm/Kadane's Algorithm.cpp
@@ -0,0 +1,11 @@
+    long long maxSubarraySum(int arr[], int n){
+        long long sum=0;
+        long long maxi=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+            maxi=max(maxi,sum);
+            if(sum<0)sum=0;
+        }
+        return maxi;
+    }
---
+++ Greedy-Kadane's Algorithm/README.md
@@ -0,0 +1,1 @@
+<h2><a href="https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1">Kadane's Algorithm</a></h2><h3>GfG-Medium</h3><hr>
